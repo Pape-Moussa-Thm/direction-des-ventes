9f413f7e761b13fb5eb1f188c92504106e314e74
Pape-Moussa-Thm <thiampapemoussa9@gmail.com>
2024-02-05 21:17:20 +0000

    direction-des-ventes

--- my_dv/direction_ventes/views.py
+++ my_dv/direction_ventes/views.py
@@ -1,10 +1,17 @@
-from django.conf import settings
+from base64 import urlsafe_b64decode
+from email.message import EmailMessage
+from django.conf import Settings
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from .tokens import generate_token
 
 
 # Create your views here.
@@ -46,18 +53,37 @@ def signup(request):
         myuser.confirmpassword = confirmpassword
         myuser.firstname = firstname
         myuser.lastname = lastname
-        
+        myuser.is_active = False
         myuser.save()
         
-        messages.success(request,"Your Account has been created successfully.")
+        messages.success(request,"Your Account has been successfully created. We have sent you a confirmation email, please confirm you email in order to activate your account.")
         
         
         # welcome Email
         subject = "Welcome to GFG - Django Login!!"
-        message = "Hello" + myuser.firstname + "!! \n" + "Welcome to GFG!! \n Thank you for visiting our website \n We have also sent you a confirmation email, please confirm your email address in order to activate your account. \n\n Thanking You\n Anubhav Madhav"
-        from_email = settings.EMAIL_HOST_USER
+        message = "Hello" + myuser.firstname + "!! \n" + "Welcome to GFG!! \n Thank you for visiting our website \n We have also sent you a confirmation email, please confirm your email address in order to activate your account. \n\n Thanking You\n Pape Thiam"
+        from_email = Settings.EMAIL_HOST_USER
         to_list = [myuser.email]
         send_mail(subject, message, from_email, to_list, fail_silently=True)
+        
+        
+        # Email Address Confirmation Email
+        
+        current_site = get_current_site(request)
+        email_subject = "confirm your email @ GFG - Django Login!!"
+        message2 = render_to_string('email_confirmation.html',{
+            'name':myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser),
+        })
+        email = EmailMessage(
+            email_subject, 
+            message2, Settings.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
         
         
         return redirect('signin')
@@ -75,7 +101,7 @@ def signin(request):
         if user is not None:
             login(request, user)
             firstname = user.firstname
-            return redirect(request,"authentication/index.html", {'firstname':firstname})
+            return redirect(request,"index.html", {'firstname':firstname})
             
         else:
             messages.error(request, "Invalid username or password")
@@ -87,3 +113,22 @@ def signout(request):
     logout(request)
     messages.success(request, "Logged Out successfully!")
     return redirect('home')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_b64decode(uidb64))
+
+        myuser = User.objects.get(pk=uid)
+    except (TypeError, ValueError,  OverflowError, User.DoesNotExist):
+        myuser = None
+        
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        login(request, myuser)
+        return redirect('home')
+    else:
+        return render(request, 'activation_failed.html')
+    
+    
